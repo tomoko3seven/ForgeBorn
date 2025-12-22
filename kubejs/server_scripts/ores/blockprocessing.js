338ded4370a3c118cac3f42ee71813a80ef666b2
tomoko3seven <sqvizers@gmail.com>
2025-12-22 23:23:05 +0100

    More CleanUp

--- kubejs/server_scripts/ores/blockprocessing.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(event => {
-    event.recipes.gtceu.centrifuge('kubejs:blacksand')
-        .itemInputs('1x biomesoplenty:black_sand')
-        .itemOutputs('1x gtceu:tiny_gold_dust', '1x gtceu:tiny_magnetite_dust', 'gtceu:tiny_biotite_dust', 'gtceu:tiny_hematite_dust', 'gtceu:tiny_rutile_dust')
-        .duration(100)
-        .EUt(5)
-})
