93164c7f6095e9fd8e30c630add4782de897104f
Silas Kinnear <silask7188@silas.kinnear@gmail.com>
2025-12-23 11:45:16 -0800

    fixed formatting and stuff

--- kubejs/server_scripts/Botania/Botania.js
+++ kubejs/server_scripts/Botania/Botania.js
@@ -6,9 +6,7 @@ ServerEvents.recipes(event => {
     // ---------------------------------------------------------
     // Removals
     // ---------------------------------------------------------
-    // Move Malum removal later if needed
     event.remove({ output: 'malum:poppet' })
-
     event.remove({ output: 'botania:mana_pool' })
     event.remove({ output: 'botania:mana_spreader' })
     event.remove({ output: 'botania:diluted_pool' })
@@ -19,8 +17,11 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'botania:manasteel_ingot' })
     event.remove({ output: 'botania:mana_powder' })
     event.remove({ output: 'botania:gaia_ingot' })
+    event.remove({ output: 'botania:livingwood_bow' })
+    event.remove({ id: 'botania:apothecary_default' })
+    event.remove({ id: 'botania:mana_infusion/manasteel_block' })
+    event.remove({ id: 'botania:mana_infusion/mana_quartz' })
 
-    // Elven Trade Removals
     const elvenRemovals = [
         'botania:elven_trade/dragonstone_block',
         'botania:elven_trade/dragonstone',
@@ -35,7 +36,6 @@ ServerEvents.recipes(event => {
     ]
     elvenRemovals.forEach(id => event.remove({ id: id }))
 
-
     // ---------------------------------------------------------
     // Nature's Aura Integration
     // ---------------------------------------------------------
@@ -45,115 +45,6 @@ ServerEvents.recipes(event => {
         'botania:purple_petal', 'naturesaura:gold_leaf'
     ], 'oak_sapling', 60)
 
-
-    // ---------------------------------------------------------
-    // Botanical Circuits & Components
-    // ---------------------------------------------------------
-    
-    // Livingrock Circuit Board
-    event.recipes.gtceu.assembler('gtceu:assembler/livingrock_circuit_board')
-        .itemInputs('8x gtceu:manasteel_foil', 'gtceu:livingrock_plate')
-        .inputFluids('kubejs:dense_mana 200')
-        .itemOutputs('kubejs:livingrock_circuit_board')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(200)
-
-    const livingrockboard = ['kubejs:mana_infused_livingrock_circuit_board', 'kubejs:livingrock_circuit_board', 3000]
-    event.recipes.botania.mana_infusion(livingrockboard[0], livingrockboard[1], livingrockboard[2])
-    event.recipes.gtceu.mana_infuser('livingrock_circuit_board_best')
-        .itemInputs(livingrockboard[1])
-        .itemOutputs(livingrockboard[0])
-        .inputFluids(`kubejs:dense_mana ${livingrockboard[2]/10}`)
-        .duration(20)
-        .EUt(GTValues.VA[GTValues.MV])
-    
-    // Mana Chip (ULV)
-    event.recipes.gtceu.assembler('kubejs:mana_chip')
-        .itemInputs('2x gtceu:manasteel_plate')
-        .itemInputs('2x gtceu:manasteel_foil')
-        .itemInputs('2x gtceu:manasteel_bolt')
-        .itemInputs('2x gtceu:fine_copper_wire')
-        .inputFluids('kubejs:dense_mana 100')
-        .itemOutputs('kubejs:mana_chip')
-        .EUt(GTValues.VA[GTValues.ULV])
-        .duration(20 * 10)
-
-    // Wafers & Chips
-    const livingrockwafer = ['kubejs:livingrock_wafer', 'gtceu:livingrock_plate', 1000]
-    event.recipes.botania.mana_infusion(livingrockwafer[0], livingrockwafer[1], livingrockwafer[2])
-    event.recipes.gtceu.mana_infuser('livingrock_wafer_best')
-        .itemInputs(livingrockwafer[1])
-        .itemOutputs(livingrockwafer[0])
-        .inputFluids(`kubejs:dense_mana ${livingrockwafer[2]/10}`)
-        .duration(20)
-        .EUt(GTValues.VA[GTValues.MV])
-
-    event.recipes.gtceu.mana_pulse_infuser('kubejs:mana_multithreaded_computation_wafer')
-        .notConsumable('gtceu:utherium_lens')
-        .itemInputs('16x kubejs:livingrock_wafer')
-        .inputFluids('kubejs:dense_mana 1600')
-        .itemOutputs('16x kubejs:mana_multithreaded_computation_wafer')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(20 * 30)
-
-    // Wafer Cutting
-    event.recipes.gtceu.cutter('kubejs:mana_multithreaded_computation_wafer_water')
-        .itemInputs('kubejs:mana_multithreaded_computation_wafer')
-        .inputFluids('minecraft:water 337')
-        .itemOutputs('8x kubejs:mmc_chip')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(280)
-
-    event.recipes.gtceu.cutter('kubejs:mana_multithreaded_computation_wafer_distilled_water')
-        .itemInputs('kubejs:mana_multithreaded_computation_wafer')
-        .inputFluids('gtceu:distilled_water 253')
-        .itemOutputs('8x kubejs:mmc_chip')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(200)
-
-    event.recipes.gtceu.cutter('kubejs:mana_multithreaded_computation_wafer_lubricant')
-        .itemInputs('kubejs:mana_multithreaded_computation_wafer')
-        .inputFluids('gtceu:lubricant 84')
-        .itemOutputs('8x kubejs:mmc_chip')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(120)
-
-
-    // ---------------------------------------------------------
-    // Mana Infusion Recipes
-    // ---------------------------------------------------------
-    const infusionRecipes = [
-        // [output, input, mana cost]
-        // ['kubejs:token_of_light', 'kubejs:light_token', 100000], doesnt exist?
-        ['kubejs:pure_mana_dust', '#botania:mana_dusts', 50000],
-        ['botania:quartz_mana', 'gtceu:quartzite_gem', 500],
-        ['gtceu:managold_ingot', '#forge:ingots/gold', 5000],
-        ['botania:mana_string', 'ars_nouveau:magebloom_fiber', 1250],
-        ['botania:manasteel_ingot', 'gtceu:blue_alloy_ingot', 3000],
-        ['botania:mana_powder', 'gtceu:lazurite_dust', 400]
-    ]
-
-    infusionRecipes.forEach(recipe => {
-        let outputItem = recipe[0]
-        let inputItem = recipe[1]
-        let manaCost = recipe[2]
-
-        let recipeId = outputItem.replace(':', '_')
-
-        event.recipes.botania.mana_infusion(outputItem, inputItem, manaCost)
-            .id(`kubejs:botania_infusion/${recipeId}`)
-
-        event.recipes.gtceu.mana_infuser(`botania_conversion/${recipeId}`)
-            .itemInputs(inputItem)
-            .itemOutputs(outputItem)
-            .inputFluids(`kubejs:dense_mana ${manaCost / 10}`)
-            .duration(20)
-            .EUt(GTValues.VA[GTValues.MV])
-    })
-
-    event.remove({ id: 'botania:mana_infusion/mana_quartz' })
-
-
     // ---------------------------------------------------------
     // Runic Altar Recipes
     // ---------------------------------------------------------
@@ -161,7 +52,6 @@ ServerEvents.recipes(event => {
         "#forge:double_plates/manasteel", "#forge:double_plates/manasteel", 'botania:manaweave_cloth'
     ], 10000)
 
-    // Colorful Quartz
     const quartzColors = [
         { id: 'botania:quartz_sunny', item: 'minecraft:sunflower' },
         { id: 'botania:quartz_dark', item: 'gtceu:coke_gem' },
@@ -185,9 +75,8 @@ ServerEvents.recipes(event => {
         '#forge:gems/wind', 'minecraft:leather_boots', '#forge:nuggets/managold'
     ], 5000)
 
-
     // ---------------------------------------------------------
-    // Machine Assembly & Crafting
+    // Standard Crafting (Apothecary, Spreader, Etc)
     // ---------------------------------------------------------
     
     // Pure Daisy
@@ -205,7 +94,6 @@ ServerEvents.recipes(event => {
     })
 
     // Apothecary
-    event.remove({ id: 'botania:apothecary_default' })
     event.shaped('botania:apothecary_default', [
         'SBS',
         'MCH',
@@ -217,13 +105,6 @@ ServerEvents.recipes(event => {
         M: '#forge:tools/mallets',
         H: '#forge:tools/hammers',
     })
-
-    // Mana Pool
-    event.recipes.gtceu.assembler('gtceu:assembler/mana_pool')
-        .itemInputs('5x gtceu:livingrock_plate', '5x botania:polished_livingrock', 'kubejs:mana_core')
-        .itemOutputs('botania:mana_pool')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(400)
 
     // Mana Spreader
     event.shaped('botania:mana_spreader', [
@@ -238,16 +119,7 @@ ServerEvents.recipes(event => {
         P: 'gtceu:livingwood_plate'
     })
 
-    // Runic Altar
-    event.recipes.gtceu.assembler('gtceu:assembler/runic_altar')
-        .itemInputs('8x gtceu:livingrock_plate', '3x botania:livingrock', 'kubejs:advanced_mana_core')
-        .inputFluids("kubejs:dense_mana 400")
-        .itemOutputs('botania:runic_altar')
-        .EUt(GTValues.VA[GTValues.LV])
-        .duration(300)
-
     // Livingwood Bow
-    event.remove({ id: 'botania:livingwood_bow' })
     event.shaped('botania:livingwood_bow', [
         'HTM',
         'TOM',
@@ -259,7 +131,6 @@ ServerEvents.recipes(event => {
         H: '#forge:tools/hammers',
         F: '#forge:tools/files'
     })
-
 
     // ---------------------------------------------------------
     // Material Processing (Cutting & Lathe)
@@ -337,49 +208,4 @@ ServerEvents.recipes(event => {
         .itemOutputs('4x gtceu:livingwood_plate')
         .EUt(GTValues.VA[GTValues.ULV])
         .duration(120)
-
-
-    // ---------------------------------------------------------
-    // Fluid & GT Machine Recipes
-    // ---------------------------------------------------------
-    
-    // Dense Mana
-    event.recipes.gtceu.chemical_reactor('dense_mana')
-        .inputFluids('blasmatech:mana 1000')
-        .outputFluids('kubejs:dense_mana 100')
-        .EUt(GTValues.VA[GTValues.ULV])
-        .duration(120)
-
-    // GT Mana Infuser (Manasteel)
-    event.recipes.gtceu.mana_infuser('manasteel_ingot_best')
-        .itemInputs('gtceu:blue_alloy_ingot')
-        .itemOutputs('botania:manasteel_ingot')
-        .inputFluids(Fluid.of('kubejs:dense_mana', 300))
-        .duration(20)
-        .EUt(GTValues.VA[GTValues.MV])
-
-    event.recipes.gtceu.mana_infuser('manasteel_block_best')
-        .itemInputs('gtceu:blue_alloy_block')
-        .itemOutputs('botania:manasteel_block')
-        .inputFluids(Fluid.of('kubejs:dense_mana', 2700))
-        .duration(20)
-        .EUt(GTValues.VA[GTValues.MV])
-
-    event.recipes.remove({id: "botania:mana_infusion/manasteel_block"})
-
-    // GT Runic Assembler
-    event.recipes.gtceu.runic_assembler('black_quartz')
-        .itemInputs('8x minecraft:quartz', 'gtceu:coke_gem')
-        .itemOutputs('botania:quartz_dark')
-        .inputFluids(Fluid.of('kubejs:dense_mana', 50))
-        .duration(200)
-        .EUt(GTValues.VA[GTValues.MV])
-
-    event.recipes.gtceu.runic_assembler("manasteel_reinforced_plating")
-        .itemInputs('2x #forge:double_plates/manasteel', 'botania:manaweave_cloth')
-        .itemOutputs('kubejs:manasteel_reinforced_plating')
-        .inputFluids(Fluid.of('kubejs:dense_mana', 300))
-        .duration(20)
-        .EUt(GTValues.VA[GTValues.MV])
-
 })
--- kubejs/server_scripts/Botania/Gregtania.js
+++ kubejs/server_scripts/Botania/Gregtania.js
@@ -1,5 +1,5 @@
-
 // priority: 0
+
 ServerEvents.recipes(event => {
 
     // ---------------------------------------------------------
@@ -10,7 +10,73 @@ ServerEvents.recipes(event => {
         .inputFluids('gtceu:mana_soldering_alloy 74')
         .itemOutputs('kubejs:basic_botanical_circuit')
         .EUt(GTValues.VA[GTValues.LV])
-        .duration(20 * 15)
+        .duration(300)
+
+    // Livingrock Circuit Board
+    event.recipes.gtceu.assembler('gtceu:assembler/livingrock_circuit_board')
+        .itemInputs('8x gtceu:manasteel_foil', 'gtceu:livingrock_plate')
+        .inputFluids('kubejs:dense_mana 200')
+        .itemOutputs('kubejs:livingrock_circuit_board')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(200)
+
+    // Infused Livingrock Board
+    event.recipes.botania.mana_infusion('kubejs:mana_infused_livingrock_circuit_board', 'kubejs:livingrock_circuit_board', 3000)
+    
+    event.recipes.gtceu.mana_infuser('livingrock_circuit_board_best')
+        .itemInputs('kubejs:livingrock_circuit_board')
+        .itemOutputs('kubejs:mana_infused_livingrock_circuit_board')
+        .inputFluids('kubejs:dense_mana 300')
+        .duration(20)
+        .EUt(GTValues.VA[GTValues.MV])
+
+    // Mana Chip (ULV)
+    event.recipes.gtceu.assembler('kubejs:mana_chip')
+        .itemInputs('2x gtceu:manasteel_plate', '2x gtceu:manasteel_foil', '2x gtceu:manasteel_bolt', '2x gtceu:fine_copper_wire')
+        .inputFluids('kubejs:dense_mana 100')
+        .itemOutputs('kubejs:mana_chip')
+        .EUt(GTValues.VA[GTValues.ULV])
+        .duration(200)
+
+    // Wafers & Chips
+    event.recipes.botania.mana_infusion('kubejs:livingrock_wafer', 'gtceu:livingrock_plate', 1000)
+    
+    event.recipes.gtceu.mana_infuser('livingrock_wafer_best')
+        .itemInputs('gtceu:livingrock_plate')
+        .itemOutputs('kubejs:livingrock_wafer')
+        .inputFluids('kubejs:dense_mana 100')
+        .duration(20)
+        .EUt(GTValues.VA[GTValues.MV])
+
+    event.recipes.gtceu.mana_pulse_infuser('kubejs:mana_multithreaded_computation_wafer')
+        .notConsumable('gtceu:utherium_lens')
+        .itemInputs('16x kubejs:livingrock_wafer')
+        .inputFluids('kubejs:dense_mana 1600')
+        .itemOutputs('16x kubejs:mana_multithreaded_computation_wafer')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(600)
+
+    // Wafer Cutting
+    event.recipes.gtceu.cutter('kubejs:mana_multithreaded_computation_wafer_water')
+        .itemInputs('kubejs:mana_multithreaded_computation_wafer')
+        .inputFluids('minecraft:water 337')
+        .itemOutputs('8x kubejs:mmc_chip')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(280)
+
+    event.recipes.gtceu.cutter('kubejs:mana_multithreaded_computation_wafer_distilled_water')
+        .itemInputs('kubejs:mana_multithreaded_computation_wafer')
+        .inputFluids('gtceu:distilled_water 253')
+        .itemOutputs('8x kubejs:mmc_chip')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(200)
+
+    event.recipes.gtceu.cutter('kubejs:mana_multithreaded_computation_wafer_lubricant')
+        .itemInputs('kubejs:mana_multithreaded_computation_wafer')
+        .inputFluids('gtceu:lubricant 84')
+        .itemOutputs('8x kubejs:mmc_chip')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(120)
 
     // ---------------------------------------------------------
     // Mana Materials & Fabrics
@@ -34,7 +100,7 @@ ServerEvents.recipes(event => {
         .inputFluids('kubejs:dense_mana 100')
         .itemOutputs('botania:manaweave_cloth')
         .EUt(GTValues.VA[GTValues.LV])
-        .duration(20 * 10)
+        .duration(200)
 
     // ---------------------------------------------------------
     // Casings
@@ -46,7 +112,7 @@ ServerEvents.recipes(event => {
         .inputFluids('kubejs:dense_mana 100')
         .itemOutputs('kubejs:manasteel_livingrock_casing')
         .EUt(GTValues.VA[GTValues.LV])
-        .duration(20 * 10)
+        .duration(200)
 
     event.recipes.gtceu.assembler('gtceu:terrasteel_livingwood_casing')
         .itemInputs('6x #forge:plates/livingwood')
@@ -55,13 +121,9 @@ ServerEvents.recipes(event => {
         .inputFluids('kubejs:dense_mana 100')
         .itemOutputs('kubejs:terrasteel_livingwood_casing')
         .EUt(GTValues.VA[GTValues.HV])
-        .duration(20 * 10)
+        .duration(200)
 
-    // ---------------------------------------------------------
-    // MV Machine Casingf
-    // ---------------------------------------------------------
     event.remove({ output: 'gtceu:mv_machine_casing' })
-
     event.recipes.gtceu.mana_pulse_infuser('mv_casing')
         .itemInputs("4x botania:manaweave_cloth")
         .itemInputs("4x gtceu:aluminium_plate")
@@ -69,31 +131,115 @@ ServerEvents.recipes(event => {
         .itemOutputs("gtceu:mv_machine_casing")
         .circuit(8)
         .EUt(GTValues.VA[GTValues.LV])
-        .duration(20 * 20)
+        .duration(400)
 
     // ---------------------------------------------------------
-    // mana infusion recipes
+    // Manual Mana Infusion Recipes
+    // ---------------------------------------------------------
+    const infusionRecipes = [
+        ['kubejs:pure_mana_dust', '#botania:mana_dusts', 50000],
+        ['botania:quartz_mana', 'gtceu:quartzite_gem', 500],
+        ['gtceu:managold_ingot', '#forge:ingots/gold', 5000],
+        ['botania:mana_string', 'ars_nouveau:magebloom_fiber', 1250],
+        ['botania:manasteel_ingot', 'gtceu:blue_alloy_ingot', 3000],
+        ['botania:mana_powder', 'gtceu:lazurite_dust', 400]
+    ]
+
+    infusionRecipes.forEach(recipe => {
+        let outputItem = recipe[0]
+        let inputItem = recipe[1]
+        let manaCost = recipe[2]
+        let recipeId = outputItem.replace(':', '_').replace('#', '')
+
+        event.recipes.botania.mana_infusion(outputItem, inputItem, manaCost)
+            .id(`kubejs:botania_infusion/${recipeId}`)
+
+        event.recipes.gtceu.mana_infuser(`botania_conversion/${recipeId}`)
+            .itemInputs(inputItem)
+            .itemOutputs(outputItem)
+            .inputFluids(`kubejs:dense_mana ${Math.floor(manaCost / 10)}`)
+            .duration(20)
+            .EUt(GTValues.VA[GTValues.MV])
+    })
+
+    // ---------------------------------------------------------
+    // Dynamic Mana Infusion Scanner
     // ---------------------------------------------------------
     event.forEachRecipe({ type: "botania:mana_infusion" }, recipe => {
-        console.warn(recipe.json.toString())
         let json = recipe.json
-        if (json.has('catalyst')) {
-            return
-        }
-        let inputItem = Ingredient.of(json.get('input'))
+        if (json.has('catalyst')) return
+        
         let outputItem = Item.of(json.get('output'))
+        if (outputItem.id == 'botania:manasteel_block') return
+
+        if (recipe.getId().includes('kubejs')) return
+
+        let inputItem = Ingredient.of(json.get('input'))
         let manaCost = json.get('mana').asInt
-        if (outputItem == Item.of('botania:manasteel_block')) {
-            return
-        }
         let uniqueId = recipe.getId().replace(':', '_').replace('/', '_')
 
         event.recipes.gtceu.mana_infuser(`botania_conversion/${uniqueId}`)
             .itemInputs(inputItem)
-            .inputFluids(`kubejs:dense_mana ${Math.max(1, manaCost / 10)}`)
+            .inputFluids(`kubejs:dense_mana ${Math.floor(Math.max(1, manaCost / 10))}`)
             .itemOutputs(outputItem)
             .EUt(GTValues.VA[GTValues.MV])
             .duration(20)
     })
+
+    // ---------------------------------------------------------
+    // Fluid & GT Machine Recipes
+    // ---------------------------------------------------------
+    
+    // Dense Mana
+    event.recipes.gtceu.chemical_reactor('dense_mana')
+        .inputFluids('blasmatech:mana 1000')
+        .outputFluids('kubejs:dense_mana 100')
+        .EUt(GTValues.VA[GTValues.ULV])
+        .duration(120)
+
+    // GT Mana Infuser (Manasteel)
+    event.recipes.gtceu.mana_infuser('manasteel_ingot_best')
+        .itemInputs('gtceu:blue_alloy_ingot')
+        .itemOutputs('botania:manasteel_ingot')
+        .inputFluids(Fluid.of('kubejs:dense_mana', 300))
+        .duration(20)
+        .EUt(GTValues.VA[GTValues.MV])
+    event.recipes.botania.mana_infusion('botania:manasteel_ingot', 'gtceu:blue_alloy_ingot', 2700*10)
+    event.recipes.gtceu.mana_infuser('manasteel_block_best')
+        .itemInputs('gtceu:blue_alloy_block')
+        .itemOutputs('botania:manasteel_block')
+        .inputFluids(Fluid.of('kubejs:dense_mana', 2700))
+        .duration(20)
+        .EUt(GTValues.VA[GTValues.MV])
+
+    // GT Runic Assembler
+    event.recipes.gtceu.runic_assembler('black_quartz')
+        .itemInputs('8x minecraft:quartz', 'gtceu:coke_gem')
+        .itemOutputs('botania:quartz_dark')
+        .inputFluids(Fluid.of('kubejs:dense_mana', 50))
+        .duration(200)
+        .EUt(GTValues.VA[GTValues.MV])
+
+    event.recipes.gtceu.runic_assembler("manasteel_reinforced_plating")
+        .itemInputs('2x #forge:double_plates/manasteel', 'botania:manaweave_cloth')
+        .itemOutputs('kubejs:manasteel_reinforced_plating')
+        .inputFluids(Fluid.of('kubejs:dense_mana', 300))
+        .duration(20)
+        .EUt(GTValues.VA[GTValues.MV])
+
+    // Mana Pool in Assembler
+    event.recipes.gtceu.assembler('gtceu:assembler/mana_pool')
+        .itemInputs('5x gtceu:livingrock_plate', '5x botania:polished_livingrock', 'kubejs:mana_core')
+        .itemOutputs('botania:mana_pool')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(400)
+
+    // Runic Altar Machine in Assembler
+    event.recipes.gtceu.assembler('gtceu:assembler/runic_altar')
+        .itemInputs('8x gtceu:livingrock_plate', '3x botania:livingrock', 'kubejs:advanced_mana_core')
+        .inputFluids("kubejs:dense_mana 400")
+        .itemOutputs('botania:runic_altar')
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(300)
 
 })
