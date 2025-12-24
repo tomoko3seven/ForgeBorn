5a10d61928498cae6324aabb7d5d2ca5cf79e034
tomoko3seven <sqvizers@gmail.com>
2025-12-24 23:47:00 +0100

    MobGrindngUtils Recipes Update

---
+++ kubejs/server_scripts/gregified/MobGrindingUtils.js
@@ -0,0 +1,22 @@
+ServerEvents.recipes(event => {
+event.remove({ id: 'mob_grinding_utils:entity_conveyor' })
+event.remove({ id: 'mob_grinding_utils:tank' })
+
+event.shaped(Item.of('4x mob_grinding_utils:entity_conveyor'), [
+    ' P ',
+    'PRP',
+    'PCP'
+], {
+    P: '#forge:plates/steel', 
+    R: 'minecraft:redstone_block', 
+    C: 'ulvcovm:ulv_conveyor_module'
+})
+event.shaped(Item.of('mob_grinding_utils:tank'), [
+    'PGP',
+    'GGG',
+    'PGP'
+], {
+    P: '#forge:plates/froststeel', 
+    G: '#forge:glass'
+})
+})
