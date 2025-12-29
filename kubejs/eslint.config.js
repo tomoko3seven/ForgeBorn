f54041e073602744725dea85a52ee47a512da3c8
Silas Kinnear <silask7188@silas.kinnear@gmail.com>
2025-12-28 23:50:48 -0800

    no 2 setup eslint, prettier, and github workflows

--- kubejs/eslint.config.js
+++ kubejs/eslint.config.js
@@ -1,18 +1,25 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     js.configs.recommended,
     {
-        ignores: ["probe/**", "node_modules/**", ".git/**"],
+        ignores: [
+            'probe/**',
+            'node_modules/**',
+            '.git/**',
+        ],
     },
     {
         rules: {
-            "no-unused-vars": "off",
-            "no-undef": "off",
-            "no-var": "error",
-            "no-useless-escape": "warn",
-            "eqeqeq": ["error", "smart"],
+            'no-unused-vars': 'off',
+            'no-undef': 'off',
+            'no-var': 'error',
+            'no-useless-escape': 'warn',
+            eqeqeq: [
+                'error',
+                'smart',
+            ],
         },
     },
 ];
